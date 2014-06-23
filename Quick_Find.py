e19c030c1bbe31a1ff45be93bf04921031559935
Sid <shetty.sud@gmail.com>
2014-06-23 00:05:42 -0400

    QF with main()

--- Quick_Find.py
+++ Quick_Find.py
@@ -32,46 +32,49 @@ def union(id_list, p, q):
         if id_list[i] == pid:
             id_list[i] = qid
 
+def main():
+    id_list = []
+    n = input('Enter the number of elements: ')
+    id_list = union_intialize(n)
 
-id_list = []
-n = input('Enter the number of elements: ')
-id_list = union_intialize(n)
 
+    #User Input
+    for i in range(1000):
+        print "\n\n"
+        print "1. To check if the objects are connected"
+        print "2. Connect the objects"
+        print "3. Print the list"
+        print "4. Done"
+        print "\nEnter your option: "
+        option = input()
 
-#User Input
-for i in range(1000):
-    print "\n\n"
-    print "1. To check if the objects are connected"
-    print "2. Connect the objects"
-    print "3. Print the list"
-    print "4. Done"
-    print "\nEnter your option: "
-    option = input()
+        if option == 1:
+            #Get the Elements for which the connection is to be found
+            p = input("Enter the First Object(No.): ")
+            q = input("Enter the Second Object(No.): ")
 
-    if option == 1:
-        #Get the Elements for which the connection is to be found
-        p = input("Enter the First Object(No.): ")
-        q = input("Enter the Second Object(No.): ")
+            Result = find(p, q)
 
-        Result = find(p, q)
+            #Spit out the result of the find connection
+            if Result is False:
+                print "\n**********Not Connected**********"
+            else:
+                print "\n**********Connected**********"
 
-        #Spit out the result of the find connection
-        if Result is False:
-            print "\n**********Not Connected**********"
-        else:
-            print "\n**********Connected**********"
+        if option == 2:
+            #Get the Elements for which the connection is to be found
+            p = input("Enter the First Object(No.): ")
+            q = input("Enter the Second Object(No.): ")
 
-    if option == 2:
-        #Get the Elements for which the connection is to be found
-        p = input("Enter the First Object(No.): ")
-        q = input("Enter the Second Object(No.): ")
+            union(id_list, p, q)
 
-        union(id_list, p, q)
+            print "\n**********Success**********"
 
-        print "\n**********Success**********"
+        if option == 3:
+            print id_list
 
-    if option == 3:
-        print id_list
+        if option == 4:
+            exit(1)
 
-    if option == 4:
-        exit(1)
+if __name__ == "__main__":
+    main()
